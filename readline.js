1c3ae7598591003e281b2591a0b427124326cf2d
Shawn Hateley <shawn.hateley@hakai.org>
2020-12-11 17:44:28 -0800

    Update readline.js

--- readline.js
+++ readline.js
@@ -1,35 +1,49 @@
 //Read the header of the master file
 const lineByLine = require('n-readlines');
-const masterFileName = "/Users/shawnhateley/Projects//SSN703US_DoseEvent.dat.master.csv"
-const master = new lineByLine(masterFileName);
-var masterHeader = readHeader(master); //Get the master file header for comparison
+//const masterFileName = "/Users/shawnhateley/Projects//SSN703US_DoseEvent.dat.master.csv"
+//const master = new lineByLine(masterFileName);
+//var masterHeader = readHeader(master); //Get the master file header for comparison
 
 var glob = require("glob") //search directory for files
 const fs = require('fs');
 
-var stations = ["SSN703","SSN844","SSN626","SSN1015"]
-var sites = ["US","DS","AS"]
+//var stations = ["SSN703","SSN844","SSN626","SSN1015"]
+//var sites = ["US","DS","AS"]
+let directory_name = "/Users/shawnhateley/Projects/MasterFileList";
+let masterList = fs.readdirSync(directory_name);
+console.log(masterList);
 
 //Get the list of files that match the station
-let fileList = {};
-fileList['files']=glob.sync("/Users/shawnhateley/Projects/Test_Data/**/" + stations[0] + sites[0] + "*", {});
-//console.log(fileList.files)
+for (var k = 0; k < masterList.length; k++) {
+  var masterFileName = directory_name + "/" + masterList[k];
+  var master = new lineByLine(masterFileName);
+  var masterHeader = readHeader(master); //Get the master file header for comparison
+
+  let fileList = {};
+  fileList['files']=glob.sync("/Users/shawnhateley/Projects/Test_Data/saltDose/**/" + masterList[k], {});
+  console.log(fileList);
+  checkFiles(fileList);
+}
 
 
-for (var i = 0; i < fileList.files.length; i++){ //do for each file in the list
-  var newFile = new lineByLine(fileList.files[i]); //check the header against the master. If it doesn't match, skip
-  var newFileHeader = readHeader(newFile);
+function checkFiles(fileList){
+  for (var i = 0; i < fileList.files.length; i++){ //do for each file in the list
+    var newFile = new lineByLine(fileList.files[i]); //check the header against the master. If it doesn't match, skip
+    var newFileHeader = readHeader(newFile);
 
-  if (masterHeader == newFileHeader){ //If the header matches, read the file in, skip the first 4 lines (header) and append to master file
-    console.log("Match");
-    var content = fs.readFileSync(fileList.files[i]).toString().split('\n');
+    if (masterHeader == newFileHeader){ //If the header matches, read the file in, skip the first 4 lines (header) and append to master file
+      console.log("Match");
+      console.log(fileList.files[i])
+      var content = fs.readFileSync(fileList.files[i]).toString().split('\n');
 
-    for (var j = 4; j < content.length; j++){
-      fs.appendFileSync(masterFileName,content[j]+'\n')
+      for (var j = 4; j < content.length; j++){
+        fs.appendFileSync(masterFileName,content[j]+'\n')
+      }
+
+    } else {
+      console.log("No Match");
+      console.log(fileList.files[i])
     }
-
-  } else {
-    console.log("No Match");
   }
 }
 
